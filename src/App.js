32ee9078ce2a8b48c80f6f7579954e8eca667572
Rebeca <rebeca.felix@hotmail.com>
2019-06-06 04:30:20 -0300

    input and button in react done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import React from 'react';
 import './App.css';
+import './Button.css';
+import Button from './components/Button';
+import Input from './components/Input';
+
 import firebase from "./firebaseConfig";
+const database = firebase.firestore();
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: "",
+      password: ""
+
+    };
+  }
+
+  handleChangeEmail = (event) => {
+    this.setState({email: event.target.value})
+  }
+
+  handleChangePassword = (event) => {
+    this.setState({password: event.target.value})
   }
 
   render() {
     return (
+     
       <div className="App">
         <header className="App-header">
-          <h1>#partiuBurgerQueen</h1>
+          <p>Burger Queen</p>
+          <Input value={this.state.email}
+            onChange={this.handleChangeEmail} 
+            text="Digite seu e-mail">
+          </Input>
+          <Input value={this.state.password}
+            onChange={this.handleChangePassword}
+            text="Digite sua senha">
+          </Input>
+          <Button text="Entrar" onClick={()=> alert(this.state.inputValue)}></Button>
         </header>
       </div>
     );
   }
 }
+
 
 export default App;
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "<sua-chave>",
-  authDomain: "<sua-chave>",
-  databaseURL: "<sua-chave>",
-  projectId: "<sua-chave>",
-  storageBucket: "<sua-chave>",
-  messagingSenderId: "<sua-chave>",
-  appId: "<sua-chave>"
+  apiKey: "AIzaSyBFwtt8xVJGn72w3ahfZZNbic9eb5SsJxo",
+  authDomain: "burger-queen-001.firebaseapp.com",
+  databaseURL: "https://burger-queen-001.firebaseio.com",
+  projectId: "burger-queen-001",
+  storageBucket: "burger-queen-001.appspot.com",
+  messagingSenderId: "1066800103541",
+  appId: "1:1066800103541:web:f1b38d4821029b25"
 };
 
 firebase.initializeApp(config);
